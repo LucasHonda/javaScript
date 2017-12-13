65174960642e75a714d1414c07095fc32106ec04
lucas <lucas.honda@gzsistemas.com.br>
2017-12-13 15:48:50 -0200

    Básico sobre strings

--- www/js/stringManipulation.js
+++ www/js/stringManipulation.js
@@ -3,3 +3,20 @@
   Arquivo para demonstrar o básico sobre strings em JavaScript
 
  */
+
+var string = "hello world";
+
+string.length;
+    => 11
+
+string.chartAt(position)
+  => string.chartAt(0);
+    => "h"
+
+string.replace(Old_text, New_text);
+  => string.replace("hello", "bye");
+    => "bye world"
+
+string.toUpperCase();
+  => string.toUpperCase();
+    => "HELLO WORLD"
